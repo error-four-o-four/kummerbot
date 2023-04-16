c6c0adc868eacb2ca4c23662b8acacc7625ea4c9
Jens B <jens.bachmayer@gmx.de>
2023-04-16 08:12:40 +0200

    update styles

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,12 @@
 import { defineConfig } from 'vite';
+import cssCustomMedia from 'postcss-custom-media';
+import cssNesting from 'postcss-nesting';
 
 export default defineConfig({
   appType: 'spa',
+  css: {
+    postcss: {
+      plugins: [cssCustomMedia(), cssNesting()],
+    },
+  },
 });
