09ad500a5eeb57960d731a3f2261e82ff609eab7
Jens B <jens.bachmayer@gmx.de>
2023-06-21 11:50:11 +0200

    feat: copy url button icon

--- src/handler/button-handler.js
+++ src/handler/button-handler.js
@@ -1,6 +1,7 @@
 import { buttonClass as contactButtonClass } from '../components/contact-list/utils.js';
 
 import contacts from '../data/contacts.js';
+import { id, useSymbol } from '../elements/svgs.js';
 
 export const buttonClass = {
   copy: 'button-copy-value',
@@ -11,16 +12,21 @@ export const buttonAttribute = {
   id: 'data-id',
 };
 
-const isCopyButton = (target) => {
-  return target.classList.contains(buttonClass.copy);
+const isCopyMailButton = (target) => {
+  return (
+    target.classList.contains(buttonClass.copy) &&
+    target.classList.contains(contactButtonClass.mail)
+  );
 };
 
-const getString = (target) => {
-  // element is share url button
-  if (!target.classList.contains(contactButtonClass.mail)) {
-    return target.value;
-  }
+const isCopyShareUrlButton = (target) => {
+  return (
+    target.classList.contains(buttonClass.copy) &&
+    !target.classList.contains(contactButtonClass.mail)
+  );
+};
 
+const getCopyMailString = (target) => {
   // element is copy contact mail button
   const id = 1 * target.getAttribute(buttonAttribute.id);
   return contacts.find((contact) => contact._id === id)?.mail || null;
@@ -36,8 +42,22 @@ export default {
 
     const { target } = e;
 
-    if (isCopyButton(target)) {
-      const string = getString(target);
+    // console.log(target);
+
+    if (isCopyShareUrlButton(target)) {
+      const string = target.value;
+      const copied = await copyData(string);
+
+      // @todo show toast message
+      const html = copied
+        ? `URL kopieren${useSymbol(id.clipboardCheck)}`
+        : `URL kopieren${useSymbol(id.clipboardX)}`;
+      target.innerHTML = html;
+      return;
+    }
+
+    if (isCopyMailButton(target)) {
+      const string = getCopyMailString(target);
       const copied = string && (await copyData(string));
 
       document
